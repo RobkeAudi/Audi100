816f04df543e56f08d688df2847d670eea907e6c
Steeewis <ugnmyk@ktu.lt>
2023-04-01 21:38:12 +0300

    Moneta nuskrenda i vieta kurioje bus tasku skaiciuokle

--- Audine100/Assets/CarController.cs
+++ Audine100/Assets/CarController.cs
@@ -84,4 +84,5 @@ public class CarController : MonoBehaviour
             rb.AddForce(-rb.velocity.normalized * 10f, ForceMode2D.Impulse);
         }
     }
+
 }
---
+++ Audine100/Assets/PickCoins.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickCoins : MonoBehaviour
+{
+    public float speed = 5f;
+    bool moveCoin;
+    GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("toCoins");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gameObject.GetComponent<Collider2D>().enabled = false;
+            moveCoin = true;
+            Destroy(gameObject, 2f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveCoin)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.transform.position, speed * Time.deltaTime);
+        }
+    }
+}
