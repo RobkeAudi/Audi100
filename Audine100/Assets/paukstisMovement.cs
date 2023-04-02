302efa6dca13f02faac863af6db2aea36bcd6ec4
Cipekaje <nnedas9@gmail.com>
2023-04-02 19:50:35 +0300

    paukstis

---
+++ Audine100/Assets/paukstisMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paukstisMovement : MonoBehaviour
+{
+
+    public GameObject pointC;
+    public GameObject pointD;
+    private Rigidbody2D rb;
+    private Transform currentPoint;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = pointD.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointD.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointD.transform)
+        {
+            flip();
+            currentPoint = pointC.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointC.transform)
+        {
+            flip();
+            currentPoint = pointC.transform;
+        }
+    }
+
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
+
