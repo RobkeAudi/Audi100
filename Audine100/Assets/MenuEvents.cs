45df13ee5f597d1e531ec87faa178f496c0af9fd
Steeewis <ugnmyk@ktu.lt>
2023-05-06 12:10:20 +0300

    Lygiu meniu ir garso nustatymas,
    Garso nustatyma reikes dar prikist ranku nes audio ne taip sudeti nzn kaip cia kas

--- Audine100/Assets/MenuEvents.cs
+++ Audine100/Assets/MenuEvents.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 
 public class MenuEvents : MonoBehaviour
 {
+    public Slider volumeSlider;
+    public AudioMixer mixer;
+    public void SetVolume()
+    {
+        mixer.SetFloat("volume", volumeSlider.value);
+        
+    }
+
+
 
     public void LoadLevel(int index)
     {
